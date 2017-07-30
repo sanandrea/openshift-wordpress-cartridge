fa5795b86db459bc51407c87ae23b3d0f580f99f
Andi Palo <sanandrea8080@gmail.com>
2017-07-30 14:23:36 +0200

    added code for meta info to validate google console

---
+++ wp-content/plugins/google-meta/add-meta-header.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Add Meta
+Plugin URI:  http://link to your plugin homepage
+Description: Describe what your plugin is all about in a few short sentences
+Version:     1.0
+Author:      Your name (Yay! Here comes fame... )
+Author URI:  http://link to your website
+License:     GPL2 etc
+License URI: http://link to your plugin license
+*/
+add_action('wp_head', 'wpse_43672_wp_head');
+function wpse_43672_wp_head(){
+    //Close PHP tags 
+    ?>
+    <meta name="google-site-verification" content="zmN0vhjamY1PnIeIbLrGFgA_JMVcaQUCQxal-s1-kGM" />
+    <?php //Open PHP tags
+}
+

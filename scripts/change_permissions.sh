33c5bc63d561c705b7429a4ce3d54881078ccbae
Andi Palo <sanandrea8080@gmail.com>
2017-09-09 17:53:50 +0100

    changed file folders

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chown -R ec2-user:apache /var/www/html/WordPress
-find /var/www/html/WordPress -type f -exec chmod -R 644 {} \;
-find /var/www/html/WordPress -type d -exec chmod -R 754 {} \;
-#chmod -R 644 /var/www/html/WordPress
-chown -R root:root /var/www/html/WordPress/scripts
-chmod -R 750 /var/www/html/WordPress/scripts 
+find /var/www/html -type f -exec chmod -R 644 {} \;
+find /var/www/html -type d -exec chmod -R 754 {} \;
+chown -R root:root /var/www/html/scripts
+chmod -R 750 /var/www/html/scripts 

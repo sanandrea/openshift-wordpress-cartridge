db32bb7430e1b5d5ee722fe82b9742a32aa9898f
Andi Palo <sanandrea8080@gmail.com>
2017-09-11 23:47:56 +0100

    remove ch permission

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -7,8 +7,8 @@ sudo scripts/write_credentials_to_conf.pl /var/www/html/wp-config.php
 sudo rm /var/www/html/scripts/ec2-user_data.txte
 
 #Fix file permissions
-sudo chown -R apache /var/www
-sudo chgrp -R apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+# sudo chown -R apache /var/www
+# sudo chgrp -R apache /var/www
+# sudo chmod 2775 /var/www
+# find /var/www -type d -exec sudo chmod 2775 {} \;
+# find /var/www -type f -exec sudo chmod 0664 {} \;

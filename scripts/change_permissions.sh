f4ea676faa47a16a3b6baabc87e24a6d87db947a
Andi Palo <sanandrea8080@gmail.com>
2017-09-12 00:04:52 +0100

    changed script again

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -2,13 +2,13 @@
 
 #Write credentials from user data to wp-config
 sudo curl http://169.254.169.254/latest/user-data > /var/www/html/scripts/ec2-user_data.txt
-sudo chmod +x scripts/write_credentials_to_conf.pl 
-sudo scripts/write_credentials_to_conf.pl /var/www/html/wp-config.php
+sudo chmod +x /var/www/html/scripts/write_credentials_to_conf.pl 
+sudo /var/www/html/scripts/write_credentials_to_conf.pl /var/www/html/wp-config.php
 sudo rm /var/www/html/scripts/ec2-user_data.txt
 
 #Fix file permissions
-# sudo chown -R apache /var/www
-# sudo chgrp -R apache /var/www
-# sudo chmod 2775 /var/www
-# find /var/www -type d -exec sudo chmod 2775 {} \;
-# find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo chown -R apache /var/www
+sudo chgrp -R apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;

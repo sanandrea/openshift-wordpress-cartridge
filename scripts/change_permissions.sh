e49711d91321d3b23c4118db6537cc918f383e82
Andi Palo <sanandrea8080@gmail.com>
2017-09-16 01:07:18 +0100

    changed ec2 instance to bitnami

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+HTDOCS_DIR="/opt/bitnami/apps/wordpress/htdocs/"
+
+
 #Write credentials from user data to wp-config
-sudo curl http://169.254.169.254/latest/user-data > /var/www/html/scripts/ec2-user_data.txt
-sudo chmod +x /var/www/html/scripts/write_credentials_to_conf.pl 
-sudo /var/www/html/scripts/write_credentials_to_conf.pl /var/www/html/wp-config.php
-sudo rm /var/www/html/scripts/ec2-user_data.txt
+sudo curl http://169.254.169.254/latest/user-data > "${HTDOCS_DIR}scripts/ec2-user_data.txt"
+sudo chmod +x "${HTDOCS_DIR}scripts/write_credentials_to_conf.pl"
+sudo "${HTDOCS_DIR}/scripts/write_credentials_to_conf.pl" "${HTDOCS_DIR}wp-config.php"
+sudo rm "${HTDOCS_DIR}scripts/ec2-user_data.txt"
 
 #Fix file permissions
-sudo chown -R apache /var/www
-sudo chgrp -R apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo chown -R bitnami $HTDOCS_DIR
+sudo chgrp -R daemon $HTDOCS_DIR
+sudo chmod 2775 $HTDOCS_DIR
+find $HTDOCS_DIR -type d -exec sudo chmod 2775 {} \;
+find $HTDOCS_DIR -type f -exec sudo chmod 0664 {} \;

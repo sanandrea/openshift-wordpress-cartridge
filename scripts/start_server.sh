45e79f139f44c413c01c459335953775665bbc9b
Andi Palo <sanandrea8080@gmail.com>
2017-09-16 01:44:41 +0100

    bitnami has its own stack

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-service httpd start
-service mysqld start
-curl http://169.254.169.254/latest/user-data -o udata.sh
-chmod +x udata.sh
-source udata.sh
+echo "starting services not implemented"
+
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-isExistApp=`pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-   service httpd stop
-else 
-	echo "The httpd service is not running. No action taken"
-fi
-
-isExistApp=`pgrep mysqld`
-if [[ -n  $isExistApp ]]; then
-    service mysqld stop
-else 
-	echo "The mysqld service is not running. No action taken"
-fi
+echo "stopping servers not implemented"
